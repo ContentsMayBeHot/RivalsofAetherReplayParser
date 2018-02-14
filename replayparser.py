837f515a17b122171e5e40084b4683317a74fd84
Rei Armenia <lazerzes@gmail.com>
2018-02-14 11:51:58 -0500

    to num numpy and to txt created

--- replayparser.py
+++ replayparser.py
@@ -5,7 +5,7 @@ import sys
 import os
 from enum import Enum
 import numpy as np
-
+import pathlib
 
 class Replay:
     def __init__(self, roa_apath):
@@ -17,16 +17,39 @@ class Replay:
         self.meta = MetaData(f_lines[0])
         self.rules = RuleData(f_lines[1])
         self.players = []
-        for i, line in enumerate(f_lines[2:]):
-            if line[0] is 'H':
+        for i, line in enumerate(f_lines):
+            if line[0] == 'H':
                 self.players.append(Player(line, f_lines[i + 1]))
+
+    def format_replay_str(self, to_file=False):
+        out_str = self.meta.format_meta_str()
+        out_str += self.rules.format_rule_str()
+
+        for player in self.players:
+            out_str += player.format_player_str(to_file)
+
+        return out_str
+
+    def create_numpy(self):
+        dir_path = self.f_name[:-4] + "/"
+        pathlib.Path(dir_path).mkdir(parents=True, exist_ok=True)
+
+        for player in self.players:
+            out_path = dir_path + player.name
+            arr = player.collapse_actions()
+            np.save(out_path, np.array(arr, dtype=object))
+
 
 
 class MetaData:
     def __init__(self, meta_line):
         self.is_starred = bool(int(meta_line[0]))
         self.version = meta_line[1:8]
-        self.date_time = meta_line[8:22]
+        self.date_time = meta_line[8:21]
+
+    def format_meta_str(self):
+        return str(self.is_starred) + "\t" + self.version + \
+            "\t" + self.date_time + "\n"
 
 
 class RuleData:
@@ -38,6 +61,10 @@ class RuleData:
         self.team = bool(int(rule_line[7]))
         self.friendly_fire = bool(int(rule_line[8]))
 
+    def format_rule_str(self):
+        return str(self.stage_type) + "\t" + str(self.stage_id) + "\t" + str(self.stock_count) + \
+            "\t" + str(self.time) + "\t" + str(self.team) + "\t" + str(self.friendly_fire) + "\n"
+
 
 class Player:
     def __init__(self, ln_info, ln_actions):
@@ -48,6 +75,14 @@ class Player:
         i = 0
         while i < len(ln_actions):
             i += self.get_single_action(i, ln_actions)
+
+    def format_player_str(self, to_file=False):
+        out_str = self.name + "\t" + str(self.character) + "\n"
+
+        for action in self.actions:
+            out_str += action.format_action_str(to_file)
+
+        return out_str
 
     def get_single_action(self, lower_bound, ln_actions):
         position = 0
@@ -84,6 +119,23 @@ class Player:
         position += 1
         return position
 
+    def collapse_actions(self):
+        out_list = []
+        u_frame = None
+        u_list = None
+
+        for action in self.actions:
+            if u_frame is None:
+                u_frame = action.frame_index
+                u_list = action.simple_matrix
+            elif u_frame is not None and u_frame == action.frame_index:
+                u_list = np.add(u_list, action.simple_matrix)
+            else:
+                out_list.append((u_frame, u_list.tolist()))
+                u_frame = action.frame_index
+                u_list = action.simple_matrix
+
+        return out_list
 
 class Action:
     def __init__(self, frame_str, input_id):
@@ -91,10 +143,30 @@ class Action:
         self.input_id = input_id
         self.type = self.cast_action()
         self.matrix = self.initialize_matrix()
+        self.simple_matrix = self.initialize_simple_matrix()
+
+    def format_action_str(self, to_file=False):
+        if not to_file:
+            return str(self.frame_index) + "\t" + str(self.type) + "\n"
+        else:
+            return str(self.frame_index) + "\t" + str(self.simple_matrix) + "\n"
 
     def initialize_matrix(self):
-        temp_array = np.zeros((1,40))
+        if self.type is ActionType.INVALID:
+            return np.zeros((40))
+
+        temp_array = np.zeros((40))
         temp_array[self.type.value] = 1
+        return temp_array
+
+    def initialize_simple_matrix(self):
+        simp = self.map_simple()
+
+        if simp is SimpleAction.INVALID:
+            return np.zeros((26))
+
+        temp_array = np.zeros((26))
+        temp_array[simp.value] = 1
         return temp_array
 
     def cast_action(self):
@@ -151,6 +223,50 @@ class Action:
             'Z': ActionType.ANG_TOGGLE_PRESS,
             'z': ActionType.ANG_TOGGLE_RELEASE
         }.get(x, ActionType.INVALID)
+
+    def map_simple(self):
+        return{
+            ActionType.LEFT_PRESS: SimpleAction.LEFT,
+            ActionType.LEFT_RELEASE: SimpleAction.LEFT,
+            ActionType.LEFT_TAP: SimpleAction.LEFT_TAP,
+            ActionType.RIGHT_PRESS: SimpleAction.RIGHT,
+            ActionType.RIGHT_RELEASE: SimpleAction.RIGHT,
+            ActionType.RIGHT_TAP: SimpleAction.RIGHT_TAP,
+            ActionType.UP_PRESS: SimpleAction.UP,
+            ActionType.UP_RELEASE: SimpleAction.UP,
+            ActionType.UP_TAP: SimpleAction.UP_TAP,
+            ActionType.DOWN_PRESS: SimpleAction.DOWN,
+            ActionType.DOWN_RELEASE: SimpleAction.DOWN,
+            ActionType.DOWN_TAP: SimpleAction.DOWN_TAP,
+            ActionType.ATTACK_PRESS: SimpleAction.ATTACK,
+            ActionType.ATTACK_RELEASE: SimpleAction.ATTACK,
+            ActionType.SPECIAL_PRESS: SimpleAction.SPECIAL,
+            ActionType.SPECIAL_RELEASE: SimpleAction.SPECIAL,
+            ActionType.JUMP_PRESS: SimpleAction.JUMP,
+            ActionType.JUMP_RELEASE: SimpleAction.JUMP,
+            ActionType.DODGE_PRESS: SimpleAction.DODGE,
+            ActionType.DODGE_RELEASE: SimpleAction.DODGE,
+            ActionType.STRONG_PRESS: SimpleAction.STRONG,
+            ActionType.STRONG_RELEASE: SimpleAction.STRONG,
+            ActionType.STRONG_LEFT_PRESS: SimpleAction.STRONG_LEFT,
+            ActionType.STRONG_LEFT_RELEASE: SimpleAction.STRONG_LEFT,
+            ActionType.STRONG_RIGHT_PRESS: SimpleAction.STRONG_RIGHT,
+            ActionType.STRONG_RIGHT_RELEASE: SimpleAction.STRONG_RIGHT,
+            ActionType.STRONG_UP_PRESS: SimpleAction.STRONG_UP,
+            ActionType.STRONG_UP_RELEASE: SimpleAction.STRONG_UP,
+            ActionType.STRONG_DOWN_PRESS: SimpleAction.STRONG_DOWN,
+            ActionType.STRONG_DOWN_RELEASE: SimpleAction.STRONG_DOWN,
+            ActionType.ANG_RIGHT: SimpleAction.ANG_RIGHT,
+            ActionType.ANG_UP_RIGHT: SimpleAction.ANG_UP_RIGHT,
+            ActionType.ANG_UP: SimpleAction.ANG_UP,
+            ActionType.ANG_UP_LEFT: SimpleAction.ANG_UP_LEFT,
+            ActionType.ANG_LEFT: SimpleAction.ANG_LEFT,
+            ActionType.ANG_DOWN_LEFT: SimpleAction.ANG_DOWN_LEFT,
+            ActionType.ANG_DOWN: SimpleAction.ANG_DOWN,
+            ActionType.ANG_DOWN_RIGHT: SimpleAction.ANG_DOWN_RIGHT,
+            ActionType.ANG_TOGGLE_PRESS: SimpleAction.ANG_TOGGLE,
+            ActionType.ANG_TOGGLE_RELEASE: SimpleAction.ANG_TOGGLE
+        }.get(self.type, SimpleAction.INVALID)
 
     def get_ms_from_start(self):
         return (self.frame_index / 60.00) * 1000
@@ -219,6 +335,44 @@ class ActionType(Enum):
     ANG_TOGGLE_RELEASE = 39
 
 
+class SimpleAction(Enum):
+    INVALID = -1
+
+    LEFT = 0
+    LEFT_TAP = 1
+
+    RIGHT = 2
+    RIGHT_TAP = 3
+
+    UP = 4
+    UP_TAP = 5
+
+    DOWN = 6
+    DOWN_TAP = 7
+
+    ATTACK = 8
+    SPECIAL = 9
+    JUMP = 10
+    DODGE = 11
+    STRONG = 12
+
+    STRONG_LEFT = 13
+    STRONG_RIGHT = 14
+    STRONG_UP = 15
+    STRONG_DOWN = 16
+
+    ANG_RIGHT = 17
+    ANG_UP_RIGHT = 18
+    ANG_UP = 19
+    ANG_UP_LEFT = 20
+    ANG_LEFT = 21
+    ANG_DOWN_LEFT = 22
+    ANG_DOWN = 23
+    ANG_DOWN_RIGHT = 24
+
+    ANG_TOGGLE = 25
+
+
 class StageType(Enum):
     INVALID = -1
     BASIC = 0
@@ -256,13 +410,23 @@ class Character(Enum):
     RANNO = 11
     CLAIREN = 12
 
+class FrameObject:
+    def __init__(self, frame, simple_matrix):
+        self.frame_index = frame
+        self.matrix = simple_matrix
+
+    def add_action(self, action):
+        self.matrix = np.add(self.matrix, action.simple_matrix)
+
+
 
 if __name__ == "__main__":
 
     passed_commands = []
-    possible_commands = ["-d", "-f", "-o"]
+    possible_commands = ["-d", "-f", "-o", "-np"]
     replays = []
-    outdir_apath = False
+    out_dir = False
+    to_np = True
 
     temp_command = []
     for arg in sys.argv:
@@ -293,13 +457,28 @@ if __name__ == "__main__":
             # TODO :: Grab all .roa files from cwd
 
         elif cmd[0] == '-o':
-            outdir_apath = True
+            out_dir = True
+
+        elif cmd[0] == '-np':
+            to_np = True
 
         else:
             print(cmd[0], "is not a supported command")
 
-        if outdir_apath:
-            print("Creating Simplified Replays in output/")
+        if out_dir:
+            print("Creating Simplified Replays")
+            for replay in replays:
+                out_path = replay.f_name[:-4]
+                out_path += "_parsed.txt"
+                f_out = open(out_path, "w+")
+                f_out.write(replay.format_replay_str(True))
+
+        if to_np:
+            print("Creating Numpy Files")
+            for replay in replays:
+                print("\tCreating file for " + replay.f_name)
+                replay.create_numpy()
 
     for replay in replays:
         print(replay.f_name)
+        print(replay.format_replay_str())
