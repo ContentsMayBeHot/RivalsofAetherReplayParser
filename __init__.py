382c5b8d9884da56f1c4af93a73559f53b919a6c
lazerzes <lazerzes@gmail.com>
2018-04-24 22:20:45 -0400

    extracted enums from module.

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .roaparser import *
---
+++ roaparser/roaemums.py
@@ -0,0 +1,144 @@
+"""ROAEnums Declaration
+"""
+from enum import Enum
+
+class ActionType(Enum):
+    INVALID = -1
+
+    LEFT_PRESS = 0
+    LEFT_RELEASE = 1
+    LEFT_TAP = 2
+
+    RIGHT_PRESS = 3
+    RIGHT_RELEASE = 4
+    RIGHT_TAP = 5
+
+    UP_PRESS = 6
+    UP_RELEASE = 7
+    UP_TAP = 8
+
+    DOWN_PRESS = 9
+    DOWN_RELEASE = 10
+    DOWN_TAP = 11
+
+    ATTACK_PRESS = 12
+    ATTACK_RELEASE = 13
+
+    SPECIAL_PRESS = 14
+    SPECIAL_RELEASE = 15
+
+    JUMP_PRESS = 16
+    JUMP_RELEASE = 17
+
+    DODGE_PRESS = 18
+    DODGE_RELEASE = 19
+
+    STRONG_PRESS = 20
+    STRONG_RELEASE = 21
+
+    STRONG_LEFT_PRESS = 22
+    STRONG_LEFT_RELEASE = 23
+
+    STRONG_RIGHT_PRESS = 24
+    STRONG_RIGHT_RELEASE = 25
+
+    STRONG_UP_PRESS = 26
+    STRONG_UP_RELEASE = 27
+
+    STRONG_DOWN_PRESS = 28
+    STRONG_DOWN_RELEASE = 29
+
+    ANG_RIGHT = 30
+    ANG_UP_RIGHT = 31
+    ANG_UP = 32
+    ANG_UP_LEFT = 33
+    ANG_LEFT = 34
+    ANG_DOWN_LEFT = 35
+    ANG_DOWN = 36
+    ANG_DOWN_RIGHT = 37
+
+    ANG_TOGGLE_PRESS = 38
+    ANG_TOGGLE_RELEASE = 39
+
+
+class SimpleAction(Enum):
+    INVALID = -1
+
+    LEFT = 0
+    LEFT_TAP = 1
+
+    RIGHT = 2
+    RIGHT_TAP = 3
+
+    UP = 4
+    UP_TAP = 5
+
+    DOWN = 6
+    DOWN_TAP = 7
+
+    ATTACK = 8
+    SPECIAL = 9
+    JUMP = 10
+    DODGE = 11
+    STRONG = 12
+
+    STRONG_LEFT = 13
+    STRONG_RIGHT = 14
+    STRONG_UP = 15
+    STRONG_DOWN = 16
+
+    ANG_RIGHT = 17
+    ANG_UP_RIGHT = 18
+    ANG_UP = 19
+    ANG_UP_LEFT = 20
+    ANG_LEFT = 21
+    ANG_DOWN_LEFT = 22
+    ANG_DOWN = 23
+    ANG_DOWN_RIGHT = 24
+
+    ANG_TOGGLE = 25
+
+
+class StageType(Enum):
+    INVALID = -1
+    BASIC = 0
+    AETHER = 1
+
+
+class Stage(Enum):
+    INVALID = -1
+    NOTHING = 0
+    TREETOP_LODGE = 1
+    FIRE_CAPITOL = 2
+    AIR_ARMADA = 3
+    ROCK_WALL = 4
+    MERCHANT_PORT = 5
+    CRASH_GAME = 6
+    BLAZING_HIDEOUT = 7
+    TOWER_HEAVEN = 8
+    TEMPEST_PEAK = 9
+    FROZEN_FORTRESS = 10
+    AETHERIAL_GATES = 11
+    ENDLESS_ABYSS = 12
+    UNAVAILABLE = 13
+    CEO_RING = 14
+    SPIRIT_TREE = 15
+    STAGE_NAME = 16
+    NEO_FIRE_CAPITAL = 17
+    SWAMPY_ESTUARY = 18
+
+
+class Character(Enum):
+    NONE = 0
+    INVALID = 1
+    ZETTERBURN = 2
+    ORCANE = 3
+    WRASTOR = 4
+    KRAGG = 5
+    FORSBURN = 6
+    MAYPUL = 7
+    ABSA = 8
+    ETALUS = 9
+    ORI = 10
+    RANNO = 11
+    CLAIREN = 12
